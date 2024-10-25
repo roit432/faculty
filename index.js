133d5d00187d08b273b4aa7cf59e8c7032a0ebda
roit432 <142099632+roit432@users.noreply.github.com>
2024-10-25 03:57:19 -0100

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ navLinks.forEach(link => {
         link.classList.add('active');
     });
 });
+
+
+const menuToggle = document.getElementById('menu-toggle');
+        const sidebar = document.getElementById('sidebar');
+
+        menuToggle.addEventListener('click', () => {
+            sidebar.classList.toggle('show');
+        });
